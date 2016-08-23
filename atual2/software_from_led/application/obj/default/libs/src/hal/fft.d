625c264f3e3da05efb0d419e546d270fba2c140c
Roberto Philippi <robertophi@gmail.com>
2016-08-23 19:19:50 -0300

    update atual 2, aceita comandos m1234 .. etc

---
+++ atual2/software_from_led/application/obj/default/libs/src/hal/fft.d
@@ -0,0 +1,18 @@
+obj/default/libs/src/hal/fft.o: libs/src/hal/fft.cpp \
+  libs/include/hal/fft.h ../application_bsp//HAL/inc/sys/alt_alarm.h \
+  ../application_bsp//HAL/inc/sys/alt_llist.h \
+  ../application_bsp//HAL/inc/alt_types.h \
+  ../application_bsp//HAL/inc/priv/alt_alarm.h \
+  ../application_bsp//HAL/inc/alt_types.h
+
+libs/include/hal/fft.h:
+
+../application_bsp//HAL/inc/sys/alt_alarm.h:
+
+../application_bsp//HAL/inc/sys/alt_llist.h:
+
+../application_bsp//HAL/inc/alt_types.h:
+
+../application_bsp//HAL/inc/priv/alt_alarm.h:
+
+../application_bsp//HAL/inc/alt_types.h:
