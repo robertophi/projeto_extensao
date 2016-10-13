f3718b8332a7eb5f3920aceb379e74f2bedf38ab
RobertoPhilippi <robertophi@gmail.com>
2016-10-13 17:45:05 -0300

    Arquivo de teste para fazer o wifi na mini-placa

---
+++ soc-quartus/testeWifi/software/appTesteWifi/obj/default/wifi_functions.d
@@ -0,0 +1,65 @@
+obj/default/wifi_functions.o: wifi_functions.c \
+  ../appTesteWifi_bsp//HAL/inc/sys/alt_stdio.h \
+  ../appTesteWifi_bsp/system.h ../appTesteWifi_bsp/linker.h \
+  ../appTesteWifi_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../appTesteWifi_bsp//HAL/inc/io.h \
+  ../appTesteWifi_bsp//HAL/inc/alt_types.h \
+  ../appTesteWifi_bsp//drivers/inc/altera_avalon_uart_regs.h \
+  ../appTesteWifi_bsp//drivers/inc/altera_avalon_uart.h \
+  ../appTesteWifi_bsp//HAL/inc/sys/termios.h \
+  ../appTesteWifi_bsp//HAL/inc/sys/alt_warning.h \
+  ../appTesteWifi_bsp//HAL/inc/os/alt_sem.h \
+  ../appTesteWifi_bsp//HAL/inc/priv/alt_no_error.h \
+  ../appTesteWifi_bsp//HAL/inc/alt_types.h \
+  ../appTesteWifi_bsp//HAL/inc/os/alt_flag.h \
+  ../appTesteWifi_bsp//HAL/inc/alt_types.h \
+  ../appTesteWifi_bsp//drivers/inc/altera_avalon_uart_fd.h \
+  ../appTesteWifi_bsp//HAL/inc/sys/alt_dev.h ../appTesteWifi_bsp/system.h \
+  ../appTesteWifi_bsp//HAL/inc/sys/alt_llist.h \
+  ../appTesteWifi_bsp//HAL/inc/alt_types.h \
+  ../appTesteWifi_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../appTesteWifi_bsp//HAL/inc/sys/alt_llist.h
+
+../appTesteWifi_bsp//HAL/inc/sys/alt_stdio.h:
+
+../appTesteWifi_bsp/system.h:
+
+../appTesteWifi_bsp/linker.h:
+
+../appTesteWifi_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../appTesteWifi_bsp//HAL/inc/io.h:
+
+../appTesteWifi_bsp//HAL/inc/alt_types.h:
+
+../appTesteWifi_bsp//drivers/inc/altera_avalon_uart_regs.h:
+
+../appTesteWifi_bsp//drivers/inc/altera_avalon_uart.h:
+
+../appTesteWifi_bsp//HAL/inc/sys/termios.h:
+
+../appTesteWifi_bsp//HAL/inc/sys/alt_warning.h:
+
+../appTesteWifi_bsp//HAL/inc/os/alt_sem.h:
+
+../appTesteWifi_bsp//HAL/inc/priv/alt_no_error.h:
+
+../appTesteWifi_bsp//HAL/inc/alt_types.h:
+
+../appTesteWifi_bsp//HAL/inc/os/alt_flag.h:
+
+../appTesteWifi_bsp//HAL/inc/alt_types.h:
+
+../appTesteWifi_bsp//drivers/inc/altera_avalon_uart_fd.h:
+
+../appTesteWifi_bsp//HAL/inc/sys/alt_dev.h:
+
+../appTesteWifi_bsp/system.h:
+
+../appTesteWifi_bsp//HAL/inc/sys/alt_llist.h:
+
+../appTesteWifi_bsp//HAL/inc/alt_types.h:
+
+../appTesteWifi_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../appTesteWifi_bsp//HAL/inc/sys/alt_llist.h:
