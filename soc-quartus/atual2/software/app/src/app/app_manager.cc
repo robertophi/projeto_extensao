3e0daad788793f02d8d25529ccc634c5835978f1
Rafael <cancian@lisha.ufsc.br>
2016-11-10 18:08:27 -0200

    refactoring model

--- soc-quartus/atual2/software/app/src/app/app_manager.cc
+++ soc-quartus/atual2/software/app/src/app/app_manager.cc
@@ -4,7 +4,12 @@ AppManager::AppManager() {
   
   audioDataHandler = new AudioHandler(timeMultiplexer, 5,5, 0,0);
   compassDataHandler = New CompassHandler(timeMultiplexer, 5,2, 0,0);
-  compassDataHandler = New CompassHandler(null, 5,3, 0,2);
+  aceceroneterDataHandler = New AceceroneterHandler(null, 5,3, 0,2);
+  
+  audioDataHandler->setNext(compassDataHandler);
+  compassDataHandler->setNext(aceceroneterDataHandler);
+  
+  handlers
   ...
 }
   
