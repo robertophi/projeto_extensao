bceefa03c2838b83af3f0dd25729f1d8c2ae50fc
Caique Marques <mqcaique@gmail.com>
2016-12-06 14:41:33 -0200

    app and fft: Some fixes

--- soc-quartus/atual2/software/application/libs/include/app.h
+++ soc-quartus/atual2/software/application/libs/include/app.h
@@ -11,105 +11,128 @@
 #define MAX_SIZE 1024
 
 struct run_param {
-	char* possible;
-	char current;
-	int iterator;
+    char* possible;
+    char current;
+    int iterator;
 
-	void init() {
-		possible = "macg";
-		current = possible[0];
-		iterator = 0;
-	}
-	void next() {
-		current = possible[iterator++];
-	}
+    void init() {
+        possible = "macg";
+        current = possible[0];
+        iterator = 0;
+    }
+    void next() {
+        iterator = (iterator +1)%4;
+        current = possible[iterator];
+    }
 };
 
 class App {
 public:
-	App();
-	~App();
+    App();
+    ~App();
 
-	void setup();
-	void run();
+    void setup();
+    void run();
 
 private:
-	int compass_vib_value;
-	class Buffer {
-		public:
-			Buffer(){
-				size = 10;
-				buffer = new unsigned char*[size];
-				begin = -1;
-				end = 0;
-				len = 0;
-			}
+    int compass_vib_value;
+    class Buffer {
+        public:
+            Buffer(){
+                size = 10;
+                buffer = new unsigned char*[size];
+                begin = -1;
+                end = 0;
+                len = 0;
+            }
 
-			Buffer(unsigned int size){
-				this->size = size;
-				buffer = new unsigned char*[this->size];
-				begin = -1;
-				end = 0;
-				len = 0;
-			}
+            Buffer(unsigned int size){
+                this->size = size;
+                buffer = new unsigned char*[this->size];
+                begin = -1;
+                end = 0;
+                len = 0;
+            }
 
-			void push(unsigned char * data) {
-				if(end == begin || begin == -1) {
-					begin++;
-				}
+            void push(unsigned char * data) {
+                if(end == begin || begin == -1) {
+                    begin++;
+                }
 
-				buffer[end++] = data;
+                buffer[end++] = data;
 
-				len = ++len%size;
-				end %= size;
-			}
+                len = ++len%size;
+                end %= size;
+            }
 
-			unsigned char * pop() {
-				if(begin == -1)
-					return NULL;
+            unsigned char * pop() {
+                if(begin == -1)
+                    return NULL;
 
-				begin = ++begin&size;
+                begin = ++begin&size;
 
-				if(begin == end) {
-					end = 0;
-					begin = -1;
-					len = 0;
-					return NULL;;
-				}
+                if(begin == end) {
+                    end = 0;
+                    begin = -1;
+                    len = 0;
+                    return NULL;;
+                }
 
-				len--;
+                len--;
 
-				return buffer[(begin - 1)];
-			}
+                return buffer[(begin - 1)];
+            }
 
-			int length() {
-				return len;
-			}
+            int length() {
+                return len;
+            }
 
-		private:
-			unsigned int size;
-			unsigned int len;
-			int begin;
-			int end;
-			unsigned char **buffer;
-		};
+        private:
+            unsigned int size;
+            unsigned int len;
+            int begin;
+            int end;
+            unsigned char **buffer;
+        };
 
-	void vibrate(void *arg);
-	void writeCompass(int direction);
-	void writeGyroscope(int xAngle, int yAngle, int zAngle);
-	int defineIndex(int value, int size);
-	int oddMotors(int value, int size);
-	int evenMotors(int value, int size);
-	void writeAudio(int* freq, int samples);
-	void compass4(int direction);
-	void compass8(int direction);
+    void vibrate(void *arg);
+    void writeCompass(int direction);
+    void compass4(int direction);
+    void compass8(int direction);
+    void writeGyroscope(int xAngle, int yAngle, int zAngle);
+    int defineIndex(int value, int size);
+    int oddMotors(int value, int size);
+    int evenMotors(int value, int size);
+    void writeAudio(int* freq, int samples);
 
-	static void fftHandler(unsigned int output);
-	static Motors *motors;
-	static FFT *fft;
-	static Buffer buffer;
-	run_param running;
+    void update_motors_settings() {
+        switch(this->running.current) {
+            case 'a':
+                motors->write((1<<24)| 255 | 255 | 5 );
+                motors->write((2<<24)| 255 | 255 | 1 );
+                break;
+            case 'c':
+                motors->write((int)( 1 | 255 | 255 | 5 ));
+                motors->write((int)( 2 | 255 | 255 | 1 ));
+                break;
+            case 'g':
+                motors->write(( (1 << 24) | 255 | 255 | 15 ));
+                motors->write(( (2 << 24) | 255 | 255 | 5 ));
+                break;
+            default:
+                motors->write(( (1 << 24) | 255 | 255 | 25 ));
+                motors->write(( (2 << 24) | 255 | 255 | 10 ));
+                break;
+        }
+    }
 
-	WiFi *wifi;};
+    static void fftHandler(unsigned int output);
+    static Motors *motors;
+    static FFT *fft;
+    static Buffer buffer;
+    run_param running;
+    unsigned int samples;
+
+    WiFi *wifi;};
 
 #endif
--- soc-quartus/atual2/software/application/libs/src/app.cpp
+++ soc-quartus/atual2/software/application/libs/src/app.cpp
@@ -6,65 +6,64 @@
 //Senha não pode ter menos de 8 caracteres
 
 /*Static*/
-Motors	*App::motors = Motors::getSingleton();
-FFT	*App::fft = FFT::getSingleton();
-App::Buffer	App::buffer;
+Motors  *App::motors = Motors::getSingleton();
+FFT *App::fft = FFT::getSingleton();
+App::Buffer App::buffer;
 
 App::App() {
-	wifi = WiFi::getSingleton();
-	compass_vib_value = 25;
-	running.init();
+    wifi = WiFi::getSingleton();
+    compass_vib_value = 25;
+    running.init();
+    samples = 1024;
 }
 
 App::~App() {
-	wifi->stopServer();
+    wifi->stopServer();
 }
 
 void App::setup() {
-	wifi->config(SSID, PASSWORD);
-	wifi->startServer();
-	//fft->setInterruptHandler(App::fftHandler);
+    wifi->config(SSID, PASSWORD);
+    wifi->startServer();
+    //fft->setInterruptHandler(App::fftHandler);
 }
 
-void App::fftHandler(unsigned int output) {
+//void App::fftHandler(unsigned int output) {
+//
+//  //  motors->write(output);
+//  fft->read();
+//
+//  if(buffer.length() > 0)
+//      fft->write((int*)buffer.pop(), samples);
+//}
 
-	//	motors->write(output);
-	fft->read();
-
-	if(buffer.length() > 0)
-		fft->write(buffer.pop());
-}
-
-//If we have 5 motors in a column, each motor is responsible for 72 degree each;
-//We receive a float in (-180 degree, 180 degree]. To simplify we add 180 to it.
-
+//If we have 5 motors in a column, each motor is responsible for 72� each; We receive a float in (-180�, 180�]. To simplify we add 180 to it.
 void App::writeCompass(int direction) {
     if(COLUMNS >= 8) {
-        return compass8(direction);
+        compass8(direction);
     } else if(COLUMNS >= 4) {
-        return compass4(direction);
+        compass4(direction);
     }
 }
 
 void App::compass4(int direction) {
     if(direction < 5 || direction > 355) {
         motors->write( 0 | LINES-2 | (COLUMNS-1)/2 | compass_vib_value );
-        if(direction < 5 && direction != 0)
+         if(direction < 5 && direction != 0)
             motors->write( 0 | LINES-1 | 1 | compass_vib_value/2 );
-        else if(direction > 355 && direction != 360)
+         else if(direction > 355 && direction != 360)
             motors->write( 0 | LINES-1 | COLUMNS-2 | compass_vib_value/2 );
         return;
     }
-    int motor = direction < 180 ? (direction < 90 ? 1 : 0) : (direction < 270 ? COLUMNS-2 : COLUMNS-1);
+    int motor = direction < 180 ? (direction < 90 ? 1 : 0) : (direction < 270 ? COLUMNS-1 : COLUMNS-2);
     motors->write( 0 | LINES-1 | motor | compass_vib_value );
 }
 
 void App::compass8(int direction) {
     if(direction < 5 || direction > 355) {
         motors->write( 0 | LINES-2 | (COLUMNS-1)/2 | compass_vib_value );
-        if(direction < 5 && direction != 0)
+         if(direction < 5 && direction != 0)
             motors->write( 0 | LINES-1 | 3 | compass_vib_value/2 );
-        else if(direction > 355 && direction != 360)
+         else if(direction > 355 && direction != 360)
             motors->write( 0 | LINES-1 | COLUMNS-4 | compass_vib_value/2 );
         return;
     }
@@ -85,174 +84,148 @@ void App::compass8(int direction) {
     motors->write( 0 | LINES-1 | motor | compass_vib_value );
 }
 
-
 void App::writeGyroscope(int xAngle, int yAngle, int zAngle) {
-        int x = defineIndex(xAngle, COLUMNS) << 16;
-        int y = defineIndex(yAngle, LINES) << 8;
-        zAngle = zAngle < 0 ? zAngle*(-1) : zAngle;
-        zAngle = zAngle > 240 ? 240 : zAngle;
-        zAngle += 10;
-        motors->write(( 0 | x | y | zAngle));
-    }
+    int x = defineIndex(xAngle, COLUMNS) << 16;
+    int y = defineIndex(yAngle, LINES) << 8;
+    zAngle = zAngle < 0 ? zAngle*(-1) : zAngle;
+    zAngle = zAngle > 240 ? 240 : zAngle;
+    zAngle += 10;
+    motors->write( 0 | x | y | zAngle );
+}
 
 int App::defineIndex(int value, int size) {
-     if(size%2 == 0) {
-         return evenMotors(value, size);
-     }
-     return oddMotors(value, size);
- }
+        if(size%2 == 0) {
+            return evenMotors(value, size);
+        }
+        return oddMotors(value, size);
+    }
 
- int App::oddMotors(int value, int size) {
-     float line_int = 200.0/(size-2);
-     int half_mot = (size-1)/2;
-     float half_int_end = line_int*(half_mot-1)/2.0;
-     if((-1)*half_int_end < value && value < half_int_end) {
-         return half_mot;
-     }
-     float mot_int_beg;
-     int x = -1;
-     if(value > 0) {
-         mot_int_beg = half_int_end;
-         for(int i = half_mot+1; i < size - 1 && x == -1; i++) {
-             if(mot_int_beg <= value && value < mot_int_beg + line_int) {
-                 x = i;
-             } else {
-                 mot_int_beg += line_int;
-             }
-         }
-         if (x == -1) {
-             return size-1;
-         }
-         return x;
-     }
-     mot_int_beg = half_int_end*(-1);
-     for(int i = half_mot-1; i > 0 && x == -1; i--) {
-         if(mot_int_beg - line_int < value && value <= mot_int_beg) {
-             x = i;
-         } else {
-             mot_int_beg -= line_int;
-         }
-     }
-     if (x == -1) {
-         return 0;
-     }
-     return x;
- }
+int App::oddMotors(int value, int size) {
+    float line_int = 200.0/(size-2);
+    int half_mot = (size-1)/2;
+    float half_int_end = line_int*(half_mot-1)/2.0;
+    if((-1)*half_int_end < value && value < half_int_end) {
+        return half_mot;
+    }
+    float mot_int_beg;
+    int x = -1;
+    if(value > 0) {
+        mot_int_beg = half_int_end;
+        for(int i = half_mot+1; i < size - 1 && x == -1; i++) {
+            if(mot_int_beg <= value && value < mot_int_beg + line_int) {
+                x = i;
+            } else {
+                mot_int_beg += line_int;
+            }
+        }
+        if (x == -1) {
+            return size-1;
+        }
+        return x;
+    }
+    mot_int_beg = half_int_end*(-1);
+    for(int i = half_mot-1; i > 0 && x == -1; i--) {
+        if(mot_int_beg - line_int < value && value <= mot_int_beg) {
+            x = i;
+        } else {
+            mot_int_beg -= line_int;
+        }
+    }
+    if (x == -1) {
+        return 0;
+    }
+    return x;
+}
 
- int App::evenMotors(int value, int size) {
-     float line_int = 200.0/(size-2);
-     if(value <= -100) {
-         return 0;
-     }
-     if(value >= 100) {
-         return size-1;
-     }
-     float int_beg = -100;
-     int x = -1;
-     for(int i = 1; i < size-1 && x == -1; i++) {
-         if(int_beg < value && value <= int_beg + line_int) {
-             x = i;
-         } else {
-             int_beg += line_int;
-         }
-     }
-     return x;
- }
+int App::evenMotors(int value, int size) {
+    float line_int = 200.0/(size-2);
+    if(value <= -100) {
+        return 0;
+    }
+    if(value >= 100) {
+        return size-1;
+    }
+    float int_beg = -100;
+    int x = -1;
+    for(int i = 1; i < size-1 && x == -1; i++) {
+        if(int_beg < value && value <= int_beg + line_int) {
+            x = i;
+        } else {
+            int_beg += line_int;
+        }
+    }
+    return x;
+}
 
 void App::writeAudio(int* freq, int samples) {
-	int commom = samples/COLUMNS;
-	int summation = 0;
-	int pos = 0;
-	motors->write_to_next_line();
-	motors->write( 0 | 0 | 255 | 0 );
-	motors->write( 2 << 24 | 0 | 255 | 1 );
-	for(int i = 0; i < samples; i++) {
-		summation += freq[i];
-		if(i%commom == commom - 1) {
-			motors->write((1<<24)| 0 | (pos << 8) |(5));
-			motors->write((  0   | 0 | (pos << 8) | summation/commom ));
-			pos++;
-			summation = 0;
-		}
-	}
+    int commom = samples/COLUMNS;
+    int summation = 0;
+    int pos = 0;
+    motors->write_to_next_line();
+    for(int i = 0; i < samples; i++) {
+        summation += freq[i];
+        if(i%commom == commom - 1) {
+            motors->write((  0   | 0 | (pos << 8) | summation/commom ));
+            pos++;
+            summation = 0;
+        }
+    }
+    delete freq;
 }
 
 void App::run() {
-	unsigned char *data;
-	unsigned int *size;
+    unsigned char *data;
+    unsigned int *size;
 
-	//Shift amount de 10, Decaimento de 15 e potencia de 255 em cada motor, durante inicialização do programa.
-	motors->write((1<<24)|(255<<16)|(255<<8)|(10));
-	motors->write((2<<24)|(255<<16)|(255<<8)|(15));
-	motors->write((0<<24)|(255<<16)|(255<<8)|(255));
+    //Shift amount de 10, Decaimento de 15 e potencia de 255 em cada motor, durante inicialização do programa.
+    motors->write((1<<24)|(255<<16)|(255<<8)|(10));
+    motors->write((2<<24)|(255<<16)|(255<<8)|(15));
+    motors->write((0<<24)|(255<<16)|(255<<8)|(255));
 
-	alt_timestamp_start();
+    alt_timestamp_start();
 
-	while (1) {
+    while (1) {
 
-		wifi->receive(data, size);
-		char type = data[0];
-		if(alt_timestamp() >= 500) {
-			running.next();
-			motors->write( 1 | 255 << 16 | 255 << 8 | 0); // setting shift amount to instantaneous
-			motors->write( 0 | 255 << 16 | 255 << 8 | 0); // setting all motors to not vibrate
-			alt_timestamp_start();
-		}
-		if(running.current != type) {
-			continue;
-		}
-		switch (type) {
-		case 'm': { /* motors */
-			int cmd = (int)(data[1]);
-			int linha = (int)(data[2]);
-			int coluna = (int)(data[3]);
-			int valor = (int)(data[4]);
-//			alt_putstr("Motors received. Sending it to the motors...\n");
-			int command = (  (cmd << 24) | (linha << 16) | (coluna << 8) | (valor) );
-//			alt_printf("Comando: %d %d %d %d = %d\n" ,cmd, linha, coluna, valor, command );
-			motors->write(command);
-		}
-			break;
-		case 'a': { /*audio*/
-//			alt_putstr("Audio received. Sending it to the FFT...\n");
-//			alt_printf("%s\n", data);
-			//buffer.push(data);
-			motors->write_to_next_line();
-			fft->write(data);
-			int* freq = fft->read();
-			writeAudio(freq, 11);
-
-//			|           _
-//			|    _     / \
-//		____|___/_\___/___\_____
-//			|\_/   \_/     \   /
-//			|               \_/
-
-			//if(!fft->isProcessing()) {
-			//	fft->write(buffer.pop());
-			//}
-
-
-		}
-			break;
-		case 'c': { /*compass*/
-//			alt_putstr("Compass received. Sending to the motors...\n");
-//			alt_printf("%s\n", data);
-
-			writeCompass((int)data[1]);
-		}
-			break;
-		case 'g': { /*gyroscope*/
-//			alt_putstr("Gyroscope received. Sending to the motors...\n");
-//			alt_printf("%s\n", data);
-			writeGyroscope(int(data[1]), int(data[2]), int(data[3]));
-		}
-			break;
-		default: {
-//			alt_putstr("Some data received. Don't know what to do...\n");
-//			alt_printf("%s\n", data);
-		}
-			break;
-		}
-	}
+        wifi->receive(data, size);
+        char type = data[0];
+        if(alt_timestamp() >= 500) {
+            running.next();
+            motors->write( 1 << 24 | 255 << 16 | 255 << 8 | 0); // setting shift amount to instantaneous
+            motors->write( 0 | 255 << 16 | 255 << 8 | 0); // setting all motors to not vibrate
+            alt_timestamp_start();
+            update_motors_settings();
+        }
+        if(running.current != type) {
+            continue;
+        }
+        switch (type) {
+        case 'm': { /* motors */
+            int cmd = (int)(data[1]);
+            int linha = (int)(data[2]);
+            int coluna = (int)(data[3]);
+            int valor = (int)(data[4]);
+            int command = (  (cmd << 24) | (linha << 16) | (coluna << 8) | (valor) );
+            motors->write(command);
+        }
+            break;
+        case 'a': { /*audio*/
+            int* c = new int[this->samples];
+            for(int i = 0; i <= samples; i++) {
+                c[i] = data[i+1];
+            }
+            fft->write(c, this->samples);
+            int* freq = fft->read();
+            writeAudio(freq, this->samples/2);
+        }
+            break;
+        case 'c': { /*compass*/
+            writeCompass((int)data[1]);
+        }
+            break;
+        case 'g': { /*gyroscope*/
+            writeGyroscope(int(data[1]), int(data[2]), int(data[3]));
+        }
+            break;
+        }
+    }
 }
